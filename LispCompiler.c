fe7fe5de1e028cb85df35ea3b85db67a5869e11c
Jesus Magana <maganazero5@gmail.com>
2014-07-12 17:14:54 -0700

    Parsing polish notation

--- LispCompiler.c
+++ LispCompiler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 /* If we are compiling on Windows compile these functions */
 #ifdef _WIN32
@@ -29,6 +30,23 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
+
+  /* Create Some Parsers */
+mpc_parser_t* Number   = mpc_new("number");
+mpc_parser_t* Operator = mpc_new("operator");
+mpc_parser_t* Expr     = mpc_new("expr");
+mpc_parser_t* Lispy    = mpc_new("lispy");
+
+/* Define them with the following Language */
+mpca_lang(MPCA_LANG_DEFAULT,
+  "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+  Number, Operator, Expr, Lispy);
+
    
   puts("ZeroLisp Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
@@ -39,10 +57,23 @@ int main(int argc, char** argv) {
     char* input = readline("ZeroLisp> ");
     add_history(input);
 
-    printf("Entered: %s\n", input);
+    /* Attempt to Parse the user Input */
+mpc_result_t r;
+if (mpc_parse("<stdin>", input, Lispy, &r)) {
+  /* On Success Print the AST */
+  mpc_ast_print(r.output);
+  mpc_ast_delete(r.output);
+} else {
+  /* Otherwise Print the Error */
+  mpc_err_print(r.error);
+  mpc_err_delete(r.error);
+}
     free(input);
     
   }
+
+  /* Undefine and Delete our Parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   
   return 0;
 }
