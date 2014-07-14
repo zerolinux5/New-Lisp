98960f3c090d6bc082f208dba353cb27802af755
Jesus Magana <maganazero5@gmail.com>
2014-07-14 11:32:39 -0700

    Also evaluate mod and power | EXTRA

--- LispCompiler.c
+++ LispCompiler.c
@@ -188,6 +188,19 @@ lval* builtin_op(lval* a, char* op) {
       }
       x->num /= y->num;
     }
+    if (strcmp(op, "%") == 0) {
+      if (y->num == 0) {
+        lval_del(x); lval_del(y);
+        x = lval_err("Division By Zero.");
+        break;
+      }
+      x->num %= y->num;
+    }
+    if (strcmp(op, "^") == 0) {
+      for(int i = 1; i < y->num; i++){
+        x->num *= x->num;
+      }
+    }
     
     /* Delete element now finished with */
     lval_del(y);
@@ -279,7 +292,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                          \
       number : /-?[0-9]+/ ;                    \
-      symbol : '+' | '-' | '*' | '/' ;         \
+      symbol : '+' | '-' | '*' | '/' | '%' | '^';\
       sexpr  : '(' <expr>* ')' ;               \
       expr   : <number> | <symbol> | <sexpr> ; \
       lispy  : /^/ <expr>* /$/ ;               \
